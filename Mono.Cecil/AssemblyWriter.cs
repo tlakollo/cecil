1922b99c7a041c364c58ddff9121271e57862b76
Jb Evain <jb@evain.net>
2017-04-03 16:11:06 -0700

    Fix handling of null constants in portable pdbs

--- Mono.Cecil/AssemblyWriter.cs
+++ Mono.Cecil/AssemblyWriter.cs
@@ -2143,6 +2143,7 @@ namespace Mono.Cecil {
 			case ElementType.SzArray:
 			case ElementType.Class:
 			case ElementType.Object:
+			case ElementType.None:
 			case ElementType.Var:
 			case ElementType.MVar:
 				signature.WriteInt32 (0);
